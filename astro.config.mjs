baad6bb01feff47c8b56a98fbb51d8117889ea56
R.Keelan <R.Keelan@gmail.com>
2025-06-12 21:10:15 -0400

    Add mermaid for diagrams

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import rehypeMermaid from 'rehype-mermaid';
+import addMermaidClass from './src/plugins/add-mermaid-classname';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://keel-inc.github.io',
   vite: {
     plugins: [tailwindcss()]
+  },
+  markdown: {
+    rehypePlugins: [
+      addMermaidClass,
+      rehypeMermaid,
+    ]
   }
 });
---
+++ src/plugins/add-mermaid-classname.ts
@@ -0,0 +1,35 @@
+import { visit, CONTINUE } from "unist-util-visit"
+import type { Plugin } from 'unified';
+import type { Root, Element } from 'hast';
+
+const visitor = (node: any) => {
+  const dataLanguageMermaid = "mermaid"
+  const typeElement = "element"
+  const tagNamePre = "pre"
+  const classMermaid = dataLanguageMermaid
+
+  const isPreElement = (node: any) => typeof node.type !== undefined && node.type === typeElement
+    && node.tagName !== undefined && node.tagName === tagNamePre
+    && node.properties !== undefined && node.properties.dataLanguage === dataLanguageMermaid
+
+  if(!isPreElement(node)) {
+    return CONTINUE
+  }
+
+  const element = node as Element
+  const properties = element.properties
+  if (!properties.className) {
+    properties.className = [classMermaid]
+  } else if (Array.isArray(properties.className)) {
+    properties.className = [...properties.className, classMermaid]
+  } else if (typeof properties.className === 'string') {
+    properties.className = [properties.className, classMermaid]
+  }
+
+  return CONTINUE
+}
+
+const addMermaidClass: Plugin<void[], Root> = () =>
+  (ast: Root) => visit(ast, visitor)
+
+export default addMermaidClass 
